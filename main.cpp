6c92d2d8f4f6f6f44383f976d476bfa59e2ac68f
vanessatrizah <125655979+vanessatrizah@users.noreply.github.com>
2024-03-24 17:36:17 +0300

    Add files via upload
    corrected version of array quiz one question one

--- main.cpp
+++ main.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 
-bool containsReplica(std::vector<int>& nums) {
-    std::unordered_set<int> distinctiveSet;
-
-    for (int num : nums) {
-        // If the element is already in the set, there is a replica
-        if (distinctiveSet.count(num) > 0) {
-            return true;
-        }
-        // Otherwise, add the element to the set
-        distinctiveSet.insert(num);
+  int removeReplicas(std::vector<int>& nums) {
+    if (nums.empty()) {//references vector of intergers and returns new lengh.
+        return 0;
     }
 
-    // No replicas found
-    return false;
+    int k = 0;
+
+    for (int i = 1; i < nums.size(); ++i) {
+        if (nums[i] != nums[k]) {
+            // Move the unique element to the next position
+            nums[++k] = nums[i];
+        }
+    }
+
+    // The new length is the unique index + 1
+    return k + 1;
 }
 
 int main() {
     // Example usage
-    std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 1};
+    std::vector<int> nums = {1, 1, 2, 2, 2, 3, 4, 4, 5}; //initialise the vector
 
-    bool result = containsReplica(nums);
+    int newLength = removeReplicas(nums);
 
-    if (result) {
-        std::cout << "The array contains replicas." << std::endl;
-    } else {
-        std::cout << "The array does not contain replicas." << std::endl;
+    std::cout << "New Length: " << newLength << std::endl;
+    std::cout << "Updated Array: ";
+    for (int i = 0; i < newLength; ++i) {
+        std::cout << nums[i] << " ";
     }
 
     return 0;
